792858cb1545ca3481bcb548ae8678fb25c3b78b
michael <michael@baeshiow.com.tw>
2020-09-24 13:53:24 +0800

    新增程式碼

--- td.py
+++ td.py
@@ -1,2 +1,7 @@
 import requests
-from bs4 import 
+from bs4 import BeautifulSoup
+url = 'http://www.e-happy.com.tw'
+html = requests.get(url)
+sp = BeautifulSoup(html.text, 'html.parser')
+sp.original_encoding
+print(sp)
