ef7be4661af020673f39e0436cfe28c91933cecf
michael <michael@baeshiow.com.tw>
2020-09-24 14:50:26 +0800

    新題目

--- test1.py
+++ test1.py
@@ -1,4 +1,12 @@
-x = 5
-x = int(x)
-while x < 10:
-    print("x 小於10哦！")
+y = 3
+while y > 0 :
+    password = input("請輸入密碼： ")
+    if password == "a123456":
+        print("恭喜你猜對了")
+        y = 0
+    else:
+        y = y - 1
+        if y > 0:
+            print("猜錯了哦！你還有", y, "次機會！")
+        else:
+            print("你沒機會了哦！")
